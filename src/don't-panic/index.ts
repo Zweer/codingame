57a1a971c9446c7697859965a0d334dfcda8608b
Niccolò Olivieri Achille <n.olivieriachille@gmail.com>
2024-12-10 18:44:58 +0100

    feat(don't-panic): :zap: Reached sub 200 in don't panic

--- src/don't-panic/index.ts
+++
@@ -1,4 +0,0 @@
-const [h, w, r, ex, ey, c, _, ec] = readline().split(' ').map(Number);
-const e = Array(ec).fill(1).map((e) => readline().split(' ').map(Number));
-
-console.error(h, w, r, ex, ey, c, e);
