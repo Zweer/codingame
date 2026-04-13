b9fc66ad958e02e59e25c400edd9dbf4618c2f81
Niccolò Olivieri Achille <n.olivieriachille@gmail.com>
2026-04-13 19:02:06 +0200

    fix(the-bridge-episode-2-codesize): :recycle: rewrite Java and Python solutions
    Java: cleaner BFS with flat int[] state and Object[] queue entries.
    Python: optimized with tuple states, numeric action indices, and
    reduced allocations for better performance within time limits.

--- src/09-codegolf-hard/the-bridge-episode-2-codesize/solution.py
+++ src/09-codegolf-hard/the-bridge-episode-2-codesize/solution.py
@@ -1,79 +1,86 @@
 import sys
 input=sys.stdin.readline
 NB=int(input());MB=int(input())
-R=[input().strip()+'.'*50 for _ in[0]*4]
+R=[input().strip()+'.'*50 for _ in'1234']
 PL=len(R[0]);F=[]
-def sim(L,x,sp,act,road):
- L=L[:]
- if act=='SPEED':sp+=1
- elif act=='SLOW':sp-=1
- if act in('SPEED','SLOW','WAIT'):
-  ns=sp if act!='JUMP'else sp
-  for i in range(4):
-   if L[i]and'0'in road[i][x:x+sp]:L[i]=0
+def sim(L0,L1,L2,L3,x,sp,a):
+ if a==0:sp+=1
+ elif a==1:sp-=1
+ if a<3:
+  for i,r in enumerate([L0,L1,L2,L3]):
+   if r and'0'in R[i][x:x+sp]:
+    if i==0:L0=0
+    elif i==1:L1=0
+    elif i==2:L2=0
+    else:L3=0
   x+=sp
- elif act=='JUMP':
-  for i in range(4):
-   if L[i]and(road[i][x]=='0'or(x+sp<len(road[i])and road[i][x+sp]=='0')):L[i]=0
+ elif a==3:
+  for i,r in enumerate([L0,L1,L2,L3]):
+   if r and(R[i][x]=='0'or(x+sp<PL and R[i][x+sp]=='0')):
+    if i==0:L0=0
+    elif i==1:L1=0
+    elif i==2:L2=0
+    else:L3=0
   x+=sp
- elif act=='UP':
+ elif a==4:
+  L=[L0,L1,L2,L3]
   if L[0]:
    for i in range(4):
-    if L[i]and'0'in road[i][x:x+sp]:L[i]=0
-   x+=sp
+    if L[i]and'0'in R[i][x:x+sp]:L[i]=0
   else:
-   for i in range(1,4):
+   for i in 1,2,3:
     if not L[i-1]and L[i]:
-     if'0'in road[i-1][x:x+sp]or'0'in road[i][x:x+sp]:L[i-1]=0;L[i]=0
+     if'0'in R[i-1][x:x+sp]or'0'in R[i][x:x+sp]:L[i-1]=L[i]=0
      else:L[i-1]=1;L[i]=0
     elif L[i-1]and L[i]:
-     if'0'in road[i-1][x:x+sp]:L[i-1]=0
-     if'0'in road[i][x:x+sp]:L[i]=0
-   x+=sp
- elif act=='DOWN':
+     if'0'in R[i-1][x:x+sp]:L[i-1]=0
+     if'0'in R[i][x:x+sp]:L[i]=0
+  L0,L1,L2,L3=L;x+=sp
+ else:
+  L=[L0,L1,L2,L3]
   if L[3]:
    for i in range(4):
-    if L[i]and'0'in road[i][x:x+sp]:L[i]=0
-   x+=sp
+    if L[i]and'0'in R[i][x:x+sp]:L[i]=0
   else:
-   for i in range(2,-1,-1):
+   for i in 2,1,0:
     if not L[i+1]and L[i]:
-     if'0'in road[i][x:x+sp]or'0'in road[i+1][x:x+sp]:L[i+1]=0;L[i]=0
+     if'0'in R[i][x:x+sp]or'0'in R[i+1][x:x+sp]:L[i+1]=L[i]=0
      else:L[i+1]=1;L[i]=0
     elif L[i+1]and L[i]:
-     if'0'in road[i+1][x:x+sp]:L[i+1]=0
-     if'0'in road[i][x:x+sp]:L[i]=0
-   x+=sp
- return L,x,sp
-ACTS=['SPEED','JUMP','WAIT','UP','DOWN','SLOW']
+     if'0'in R[i+1][x:x+sp]:L[i+1]=0
+     if'0'in R[i][x:x+sp]:L[i]=0
+  L0,L1,L2,L3=L;x+=sp
+ return L0,L1,L2,L3,x,sp
+AN=['SPEED','SLOW','WAIT','JUMP','UP','DOWN']
 while 1:
  if F:
-  sp=int(input())
-  for _ in[0]*NB:input()
-  print(F.pop(0))
-  continue
+  input()
+  for _ in range(NB):input()
+  print(F.pop(0));continue
  sp=int(input());L=[0]*4;bx=0
- for _ in[0]*NB:
+ for _ in range(NB):
   x,y,a=map(int,input().split())
   L[y]=a
   if a:bx=x
- q=[(L,bx,sp,[])]
- best=None
- for _ in range(9999):
+ # BFS: state = (L0,L1,L2,L3,x,sp,actions_tuple)
+ q=[(L[0],L[1],L[2],L[3],bx,sp,())]
+ best=None;bn=0
+ for _ in range(30):
   if not q:break
   nq=[]
-  for cl,cx,cs,ca in q:
-   for act in ACTS:
-    nl,nx,ns=sim(cl,cx,cs,act,R)
-    nb=sum(nl)
+  for s in q:
+   l0,l1,l2,l3,cx,cs,ca=s
+   for ai in range(6):
+    r0,r1,r2,r3,nx,ns=sim(l0,l1,l2,l3,cx,cs,ai)
+    nb=r0+r1+r2+r3
     if nb<MB or ns<1:continue
-    na=ca+[act]
+    na=ca+(ai,)
     if nx>=PL-30:
-     if best is None or nb>sum(best[0])or(nb==sum(best[0])and nx>best[1]):best=(nl,nx,ns,na)
+     if best is None or nb>bn:best=na;bn=nb
      continue
-    nq.append((nl,nx,ns,na))
-  nq.sort(key=lambda t:-(sum(t[0])*t[1]+{'SPEED':.6,'UP':.5,'DOWN':.5,'WAIT':.3,'JUMP':.2,'SLOW':.1}.get(t[3][-1],0)))
+    nq.append((r0,r1,r2,r3,nx,ns,na))
+  nq.sort(key=lambda t:-(t[0]+t[1]+t[2]+t[3])*t[4])
   q=nq[:200]
   if best:break
- F=best[3]if best else['SPEED']
+ F=[AN[i]for i in best]if best else['SPEED']
  print(F.pop(0))
